9919968a2e00a81b624b3808debe17a3b3ded15a
EGGzin0402 <eliel.a.m.godoy@gmail.com>
2022-11-23 01:50:24 -0300

    Criação do index

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Login</title>
+
+        <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+        <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet"
+            integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous">
+        </script>
+
+        <title>Document</title>
+
+    </head>
+
+    <body>
+
+        <h1>Para qual site deseja ir?</h1>
+        <a href="BD_Produto/View/Login.php"><button name="btproduto" type="submit"
+                class="btn btn-success">Produto</button></a>
+        <a href="BD_Escola/View/LoginEsc.php"><button name="btescola" type="submit"
+                class="btn btn-dark">Escola</button></a>
+
+    </body>
+
+</html>
