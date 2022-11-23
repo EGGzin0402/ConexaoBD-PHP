8f84c290fff4ca68676a2c6927bce2f88c3dd4bc
EGGzin0402 <eliel.a.m.godoy@gmail.com>
2022-11-23 11:06:53 -0300

    heroku db

--- BD_Escola/Controler/conectar.php
+++ BD_Escola/Controler/conectar.php
@@ -1,55 +1,37 @@
 <?php
 
-class Conectar extends PDO{
-
+class Conectar extends PDO
+{
     private static $instancia;
     private $query;
-    private $host = "127.0.0.1";
-    private $usuario = "root";
-    private $senha = "";
-    private $db = "bd_escola";
+    private $host = "us-cdbr-east-06.cleardb.net";
+    private $usuario = "bd5ed7eaf61adf";
+    private $senha = "84ca6210";
+    private $db = "heroku_df28610eabc2cf1";
 
     public function __construct()
     {
-
-        parent::__construct("mysql:host=$this->host;dbname=$this->db", "$this->usuario","$this->senha");
-        
+        parent::__construct("mysql:host=$this->host;dbname=$this->db", "$this->usuario", "$this->senha");
     }
     public static function getInstance()
     {
-
-        if(!isset(self::$instancia))
-        {
-
-            try{
-
-                self::$instancia = new Conectar;
+        if (!isset(self::$instancia)) {
+            try {
+                self::$instancia = new Conectar();
                 echo 'Conectado com sucesso!';
-
-            }
-            catch (Exception $e)
-            {
-
+            } catch (Exception $e) {
                 echo 'Erro ao conectar...';
                 exit();
-
             }
-
         }
         return self::$instancia;
-
     }
     public function sql($query)
     {
-
         $this->getInstance();
         $this->query = $query;
         $stmt = $pdo ->prepare($this->query);
         $stmt->execute();
         $pdo = null;
-
     }
-
 }
-
-?>
